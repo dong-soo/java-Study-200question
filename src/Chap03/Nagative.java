6da50e6887a86ebca68e56866dcd93dae38fa979
dong-soo <68644276+dong-soo@users.noreply.github.com>
2020-10-27 01:10:33 +0900

    chap03

---
+++ src/Chap03/Nagative.java
@@ -0,0 +1,19 @@
+package Chap03;
+
+import java.util.Scanner;
+
+public class Nagative {
+
+	public static void main(String[] args) {
+		Scanner stdIn = new Scanner(System.in);
+		
+		System.out.print("정숫값 : ");
+		int a = stdIn.nextInt();
+		
+		if (a<0)
+		
+		System.out.println("이 값은 음의 값입니다.");
+		
+	}
+
+}
